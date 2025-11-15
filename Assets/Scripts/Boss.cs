952850bc4011a4151b9470656f673fd40597356a
Julian Geheeb <julian.geheeb+github@gmail.com>
2025-11-15 12:06:04 +0100

    Add boss health

---
+++ Assets/Scripts/Boss.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using UnityEngine;
+
+public class Boss : MonoBehaviour
+{
+    public event Action<float, float> OnHealthChange;
+    
+    [SerializeField] private float maxHealth;
+    
+    public float MaxHealth => maxHealth;
+
+    public float CurrentHealth
+    {
+        get => _currentHealth;
+        private set
+        {
+            _currentHealth = Mathf.Clamp(value, 0, maxHealth);
+            OnHealthChange?.Invoke(_currentHealth, maxHealth);
+        }
+    }
+
+    private float _currentHealth;
+
+    private void Awake()
+    {
+        CurrentHealth =  maxHealth;
+    }
+
+    public void Parry()
+    {
+        CurrentHealth -= 10;
+    }
+}
